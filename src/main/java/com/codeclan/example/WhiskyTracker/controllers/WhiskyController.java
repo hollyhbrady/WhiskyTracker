5bd71b7dcdccc1a568a0f4d28f0dc8b2beee8cb8
hollyhbrady <hollyhbrady@gmail.com>
2021-06-23 15:28:27 +0100

    Initial commit

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -0,0 +1,35 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam(name = "year", required = false) int year){
+        if (year != 0) {
+            return new ResponseEntity<>(whiskyRepository.findByYearEquals(year), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/{id}")
+    public ResponseEntity getWhisky(@PathVariable Long id){
+        return new ResponseEntity<>(whiskyRepository.findById(id), HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/")
+
+
+
+}
